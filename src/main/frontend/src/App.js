20381ccebdcaf565a629293b648bcae472114704
Levi Schwartzberg <Levischwartzberg@gmail.com>
2022-01-04 09:57:30 -0600

    Changed the router from browser router to hash router

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/header.jsx';
